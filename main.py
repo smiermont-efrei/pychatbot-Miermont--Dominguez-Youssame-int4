79bd35867bb97ed7f289482f4359033a910abf0e
smiermont-efrei <sophie.miermont--dominguez@efrei.net>
2023-11-17 15:23:29 +0100

    Fonction liste de présidents complète

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ def extract_president_names(file_names):
     president_names = set()
     for file_name in file_names:
         parts = file_name.split('_')
-        if len(parts) >= 3:
-            president_names.add(parts[2])
+        name = parts[1]
+        if ord(name[-1]) >= 48 and ord(name[-1]) <= 57:
+            name = ''
+            for i in range(len(parts[1][-1])-1):
+                name += str(parts[1][i])
+        president_names.add(name)
 
     return list(president_names)
 
-directory = "./speeches"
+directory = "./speeches-20231110"
 files_names = os.listdir(directory)
 president_names = extract_president_names(files_names)
 print("President Names:", president_names)
