0c0ae8f5c37ee13ec392cb027454f28ae38e727c
joyle <joy.youssame-mbili@efrei.net>
2023-11-17 21:45:35 +0100

    I converted the text

--- main.py
+++ main.py
@@ -17,3 +17,34 @@ directory = "./speeches-20231110"
 files_names = os.listdir(directory)
 president_names = extract_president_names(files_names)
 print("President Names:", president_names)
+
+
+import os
+import string
+
+def clean_text(file_path, output_directory):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    content_lower = content.lower()
+
+    translator = str.maketrans('', '', string.punctuation)
+    content_no_punct = content_lower.translate(translator)
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    cleaned_file_path = os.path.join(output_directory, os.path.basename(file_path))
+
+    with open(cleaned_file_path, 'w', encoding='utf-8') as cleaned_file:
+        cleaned_file.write(content_no_punct)
+
+speeches_directory = "./speeches-20231110"
+cleaned_directory = "./cleaned"
+
+speech_files = [file for file in os.listdir(speeches_directory) if file.endswith('.txt')]
+
+for speech_file in speech_files:
+    speech_file_path = os.path.join(speeches_directory, speech_file)
+    clean_text(speech_file_path, cleaned_directory)
+
