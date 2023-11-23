63196505cf4b3cf485c4c1e79731e1d611d35874
smiermont-efrei <sophie.miermont--dominguez@efrei.net>
2023-11-23 13:52:22 +0100

    On a corrigé plusieurs fonctions.

--- main.py
+++ main.py
@@ -1,92 +1,97 @@
-import os
+from os import *
+from string import *
+from math import *
+from collections import *
 
-
+#Fonction 1
 def extract_president_names(file_names):
     """returns a list of the names of each president from the speech file"""
     president_names = set()
     for file_name in file_names:
         parts = file_name.split('_')
-        name = parts[1]
-        if 48 <= ord(name[-1]) <= 57:
+        name1 = ''
+        for i in range(len(parts[1])-4):
+            name1 += parts[1][i]
+
+        if 48 <= ord(name1[-1]) <= 57:
             name = ''
-            for i in range(len(parts[1]) - 1):
-                name += str(parts[1][i])
+            for i in range(len(name1) - 1):
+                name += str(name1[i])
+        else:
+            name = name1
         president_names.add(name)
 
     return list(president_names)
 
-
-directory = "./speeches-20231110"
-files_names = os.listdir(directory)
+speeches_directory = "./speeches-20231110"
+files_names = listdir(speeches_directory)
 president_names = extract_president_names(files_names)
-print("President Names:", president_names)
+#print("President Names:", president_names)
 
-import os
-import string
-
-
+'''
+#fonction 2 ///////////////////////////////////////////////////////////
 def clean_text(file_path, output_directory):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    """converts the speeches to lowercase and removes the punctuation"""
+    with open(file_path, 'r') as file:
         content = file.read()
 
-    content_lower = content.lower()
+        content_lower = content.lower()
 
-    translator = str.maketrans('', '', string.punctuation)
+    translator = str.maketrans('', '', str.punctuation)
     content_no_punct = content_lower.translate(translator)
 
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+    if not path.exists(output_directory):
+        makedirs(output_directory)
 
-    cleaned_file_path = os.path.join(output_directory, os.path.basename(file_path))
+    cleaned_file_path = path.join(output_directory, path.basename(file_path))
 
-    with open(cleaned_file_path, 'w', encoding='utf-8') as cleaned_file:
+    with open(cleaned_file_path, 'w') as cleaned_file:
         cleaned_file.write(content_no_punct)
 
 
-speeches_directory = "./speeches-20231110"
-cleaned_directory = "./cleaned"
+cleaned_directory = "/cleaned"
+'''
+# We fill speech_files to make it a list of all speech files
+speech_files = []
+for file in listdir(speeches_directory):
+    if file.endswith('.txt'):
+        speech_files.append(file)
+'''
 
-speech_files = [file for file in os.listdir(speeches_directory) if file.endswith('.txt')]
-
+# We make a path for each speech to use it as the input directory, and cleaned_directory is the path the 'cleaned' files will be put into
 for speech_file in speech_files:
-    speech_file_path = os.path.join(speeches_directory, speech_file)
+    speech_file_path = path.join(speeches_directory, speech_file)
     clean_text(speech_file_path, cleaned_directory)
 
+# on a des problèmes ici //////////////////////////////////////////
+'''
 
+# Fonction 3
 def count_word_occurrences(text):
     words = text.split()
-
     word_counts = Counter(words)
-
     return word_counts
 
 
-text_example = "This is an example text. Example text is here."
-word_occurrences = count_word_occurrences(text_example)
-print("Word Occurrences:", word_occurrences)
+#text_example = "this is an example text example text is here"
+#word_occurrences = count_word_occurrences(text_example)
+#print("Word Occurrences:", word_occurrences)
 
-import os
-import math
+
 
 
 def calculate_idf(directory):
     """Get the list of file names in the directory"""
-    file_names = [file for file in os.listdir(directory) if file.endswith('.txt')]
-
-    num_documents = len(file_names)
-
+    num_documents = len(speech_files)
     idf_scores = {}
+    for file_name in speech_files:
+        file_path = path.join(directory, file_name)
 
-    for file_name in file_names:
-        file_path = os.path.join(directory, file_name)
-
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
         words = content.split()
 
-        unique_words = set(words)
-
-        for word in unique_words:
+        for word in words:
             idf_scores[word] = idf_scores.get(word, 0) + 1
 
     for word, count in idf_scores.items():
@@ -99,10 +104,8 @@ speeches_directory = "./cleaned"
 idf_scores = calculate_idf(speeches_directory)
 print("IDF Scores:", idf_scores)
 
-import os
-import math
-from collections import Counter
 
+'''
 def calculate_tf_idf_matrix(directory):
     file_names = [file for file in os.listdir(directory) if file.endswith('.txt')]
 
@@ -129,4 +132,4 @@ def calculate_tf_idf_matrix(directory):
 speeches_directory = "./cleaned"
 tf_idf_matrix = calculate_tf_idf_matrix(speeches_directory)
 print("TF-IDF Matrix:", tf_idf_matrix)
-
+'''
